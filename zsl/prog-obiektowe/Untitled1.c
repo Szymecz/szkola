7aabc8382fe85a0dc58cb035ea19310aaf6dddcb
Szymecz <67339843+Szymecz@users.noreply.github.com>
2021-09-09 15:38:29 +0200

    Update Untitled1.c

--- zsl/prog-obiektowe/Untitled1.c
+++ zsl/prog-obiektowe/Untitled1.c
@@ -1,14 +1,36 @@
 #include <iostream>
 using namespace std;
 
-class Test{
-    public a:
-        int a;
+
+
+class Worker{
+public:
+        string name;
+        string surname;
+        string nationality;
+        unsigned short int  yearBirthday;
+        float height;
+        char gender;
+
+    void showNameSurname(){
+cout << "Imię i nazwisko: "<< name << " " << surname;
 };
 
-int main(int argc, char** argv) {
-    Test newObject();
-    cout << newObject.a;
+void showAllData(){
+    cout << "\n\nImię i nazwisko: " << name << " " << surname <<  "\n" << "Narodowość:" << nationality << ", rok urodzenia: " << yearBirthday << "\nWzrost:" << height << ", płeć:" << gender;
+    };
 
+
+};
+
+
+int main(int argc, char** argv) {
+    setlocale(LC_CTYPE, "Polish");
+    Worker pracownik;
+    pracownik.name="Jan";
+    pracownik.surname="Kowalski";
+    cout << pracownik.name << endl << endl;
+    pracownik.showNameSurname();
+    pracownik.showAllData();
 }
 
