43b873a24f62c32048380422de3a84d5ac7c1524
Szymecz <67339843+Szymecz@users.noreply.github.com>
2021-09-09 15:38:08 +0200

    Add files via upload

---
+++ zsl/prog-obiektowe/Untitled1.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Test{
+    public a:
+        int a;
+};
+
+int main(int argc, char** argv) {
+    Test newObject();
+    cout << newObject.a;
+
+}
+
